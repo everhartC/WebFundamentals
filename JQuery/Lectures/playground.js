202051ba7560c3000883f17b3d0d519a7c3cd176
Cameron Everhart <cameron.r.everhart@gmail.com>
2021-01-11 14:21:20 -0800

    Adding the JQuery folder with assignments, lecture files, and Optional folder

---
+++ JQuery/Lectures/playground.js
@@ -0,0 +1,5 @@
+var path = 'img/',
+    imgs = ['cat0.png','cat1.png','cat2.png','cat3.png','cat4.png',
+    'ninja0.png','ninja1.png','ninja2.png','ninja3.png','ninja4.png'],
+    i = Math.floor(Math.random()*imgs.length);
+console.log(i);
